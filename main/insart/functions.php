d2864d95ab4bb485d80d277eee9eb352b114ff71
DESKTOP-QNA868E\befre <befrees.svl@gmail.com>
2018-02-02 16:09:12 +0200

    Редирект с аттачей и мелкие правки

--- main/insart/functions.php
+++ main/insart/functions.php
@@ -1,5 +1,20 @@
 <?php
 
+// Редирект со страницы атача
+function myprefix_redirect_attachment_page() {
+    if ( is_attachment() ) {
+        global $post;
+        if ( $post && $post->post_parent ) {
+            wp_redirect( esc_url( get_permalink( $post->post_parent ) ), 301 );
+            exit;
+        } else {
+            wp_redirect( esc_url( home_url( '/' ) ), 301 );
+            exit;
+        }
+    }
+}
+add_action( 'template_redirect', 'myprefix_redirect_attachment_page' );
+
 // Отключаем админ панель для всех пользователей
 show_admin_bar(false);
 register_sidebar(array(
